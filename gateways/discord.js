b4319ccc2f8fd0bcc9d2569755c4ff35961daf7e
Gabriel Henrique <gabrielhenrique.dev@outlook.com>
2019-08-09 19:52:49 -0300

    feat add youtubeSearch with ytdl and play stream

--- gateways/discord.js
+++ gateways/discord.js
@@ -2,13 +2,23 @@ const config = require('../config/discord')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const robots = {
-    hello: require('../robots/hello')
+    hello: require('../robots/hello'),
+    youtubeSearch: require('../robots/youtubeSearch')
 }
+
+const {google} = require('googleapis');
+const ytdl = require('ytdl-core');
+
+// initialize the Youtube API library
+const youtube = google.youtube({
+  version: 'v3',
+  auth: config.credentials.google_api,
+});
 
 
 // TODO: base interface
 function gateway(){
-    console.log("Starting Discord Gateway...")    
+    console.log("Starting Discord Gateway...")
     login()
 
     registerEvents()
@@ -20,17 +30,41 @@ function registerEvents(){
   })
 
   client.on('message', async msg => {
+    //Ignorar se a mensagem vier de um bot
+    if (msg.author.bot) return;
+
     if (msg.mentions.members.first().user.id !== client.user.id) {
       console.log('Mencionou alguém, mas não fui eu');
       return;
     }
-    if (msg.content.indexOf('fala') > -1) {
+
+    if (msg.content.indexOf('tocar') > -1) {
       if (!msg.member.voiceChannel) {
-        return msg.reply('Não posso falar, você não está em um voice channel!');
+        return msg.reply('Não posso tocar, você não está em um voice channel!');
       }
       const connection = await msg.member.voiceChannel.join();
-      const dispatcher = connection.playFile('/home/andersonef/Projects/botnator/extras/sample.mp3');
+      const lastIndex = msg.content.lastIndexOf('tocar') + 1 + ('tocar'.length);
+      const search = msg.content.substr(lastIndex);
+      console.log(`Parametro de busca: ${search}`);
+
+      const link = await robots.youtubeSearch(search);
+      console.log(`Link encontrado: ${link}`);
+
+      const dispatcher = connection.playStream(ytdl(link));
+
+      dispatcher.on('end', () => {
+        connection.disconnect();
+      });
     }
+
+    if(msg.content.indexOf('pesquisar') > -1) {
+      const lastIndex = msg.content.lastIndexOf('pesquisar') + 1 + ('pesquisar'.length);
+      const search = msg.content.substr(lastIndex);
+      const link = await robots.youtubeSearch(search);
+      msg.reply(`encontrei isso daqui no youtube: ${link}`);
+    }
+
+
     for (let indexOfRobot in robots) {
       robots[indexOfRobot](
         (callback, commands) => {
@@ -38,7 +72,7 @@ function registerEvents(){
           if (commands.indexOf(msg.content) > -1) {
             callback(msg.content);
           }
-        }, 
+        },
         (response) => {
           msg.reply(response);
         }
