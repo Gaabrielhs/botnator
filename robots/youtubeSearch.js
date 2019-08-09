756ca764b256a7e3ce19feba351d4b6f17b9acd0
Gabriel Henrique <gabrielhenrique.dev@outlook.com>
2019-08-09 19:26:23 -0300

    feat: bot youtubeSearch

---
+++ robots/youtubeSearch.js
@@ -0,0 +1,31 @@
+/*
+ *  
+ *  09/08/2019 19:20 by Gaabrielhs
+ *  Return a youtube url from searchQuery using googleapis
+ * 
+ */
+
+const {google} = require('googleapis');
+const auth_token = require('../config/discord.json').credentials.google_api;
+
+const youtube = google.youtube({
+    version: 'v3',
+    auth: auth_token
+});
+
+const base_url = 'https://www.youtube.com/watch?v=';
+
+async function search (searchQuery) {
+    const res = await youtube.search.list({
+        part: 'id',
+        maxResults: 1,
+        q: searchQuery
+    });
+
+    if(res.data.items.length == 0) return null;
+
+    const videoId = res.data.items[0].id.videoId;    
+    return base_url + videoId;
+}
+
+module.exports = search;
