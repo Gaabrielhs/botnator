0c2cb21c28f628dd2cb5aabc5c23d7e1171224cc
Gabriel Henrique <gabrielhenrique.dev@outlook.com>
2019-08-10 18:17:53 -0300

    feat: player queue

--- gateways/discord.js
+++ gateways/discord.js
@@ -3,12 +3,9 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const robots = {
     hello: require('../robots/hello'),
-    youtubeSearch: require('../robots/youtubeSearch')
+    youtubeSearch: require('../robots/youtubeSearch'),
+    player: require('../robots/player')
 }
-const ytdl = require('ytdl-core');
-
-
-
 
 // TODO: base interface
 function gateway(){
@@ -26,7 +23,7 @@ function registerEvents(){
     //Ignorar se a mensagem vier de um bot
     if (msg.author.bot) return;
 
-    if (msg.mentions.members.first().user.id !== client.user.id) {
+    if (msg.mentions.members.first() && msg.mentions.members.first().user.id !== client.user.id) {
       console.log('Mencionou alguém, mas não fui eu');
       return;
     }
@@ -35,34 +32,8 @@ function registerEvents(){
       msg.reply(`Pong!`);
     }
 
-    if (msg.content.indexOf('tocar') > -1) {
-      if (!msg.member.voiceChannel) {
-        return msg.reply('Não posso tocar, você não está em um voice channel!');
-      }
-      const connection = await msg.member.voiceChannel.join();
-      const lastIndex = msg.content.lastIndexOf('tocar') + 1 + ('tocar'.length);
-      const search = msg.content.substr(lastIndex);
-      console.log(`Parametro de busca: ${search}`);
-
-      let link = search;
-      if(!ytdl.validateURL(link)){
-        link = await robots.youtubeSearch(search);
-      }
-      console.log(`Link encontrado: ${link}`);
-
-      const dispatcher = connection.playStream(ytdl(link));
-
-      dispatcher.on('start', () => {
-        msg.reply(`tocando: ${link}`);
-      });
-
-      dispatcher.on('end', () => {
-        connection.disconnect();
-      });
-
-      dispatcher.on('error', e => {
-        console.log(e);
-      })
+    if (msg.content.indexOf('tocar') > -1 || msg.content.indexOf('pular') > -1 || msg.content.indexOf('parar') > -1) {
+      robots.player(msg);
     }
 
     if(msg.content.indexOf('pesquisar') > -1) {
@@ -72,26 +43,39 @@ function registerEvents(){
       msg.reply(`encontrei isso daqui no youtube: ${link}`);
     }
 
+    if(msg.content.indexOf('gado') > -1) {
+      if (!msg.member.voiceChannel) {
+        return msg.reply('Não posso tocar, você não está em um voice channel!');
+      }
+      const connection = await msg.member.voiceChannel.join().catch(e => {
+        msg.channel.send(`Não consegui me conectar ao seu canal de voz`);
+        return
+      });
 
-    for (let indexOfRobot in robots) {
-      robots[indexOfRobot](
-        (callback, commands) => {
-          console.log('mensagem recebida =>>> ' + msg.content);
-          if (commands.indexOf(msg.content) > -1) {
-            callback(msg.content);
-          }
-        },
-        (response) => {
-          msg.reply(response);
-        }
-      )
+      const dispatcher = connection.playFile('/home/ghs/gado.mp3');
+
+      dispatcher.on('start', () => {
+        msg.reply(`tocando rei do gado`);
+      });
+
+      dispatcher.on('end', () => {
+        connection.disconnect();
+      });
+
+      dispatcher.on('error', e => {
+        console.log(e);
+      });
+
+
     }
   })
 }
 
+
 async function login(){
-    console.log("Login discord api")
-    await client.login(config.credentials.bot_token);
+  console.log("Login discord api")
+  await client.login(config.credentials.bot_token);
 }
+
 
 module.exports = gateway
