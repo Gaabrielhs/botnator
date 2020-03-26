5746282d17a75bd5d698362203f9d9f3243f75ad
Gabriel Henrique <gabrielhenrique.dev@gmail.com>
2020-03-26 13:01:57 -0300

    [WIP] Parabens feature

---
+++ src/commands/parabens.js
@@ -0,0 +1,26 @@
+const command = 'parabens'
+
+async function execute(msg, data, args) {
+    if(msg.author.id != process.env.ADMIN_ID){
+        throw "Comando só pode ser executado por um admin"
+    }
+
+    const parar = data.commandsMap.get('parar')
+    await parar(msg, data, args)
+
+    const tocarCommand = data.commandsMap.get('tocar')
+    const newArgs = [
+        "https://www.youtube.com/watch?v=6FhNsxb0C-k"
+    ]
+    await tocarCommand(msg, data, newArgs)
+
+    const aniversariante = args[0]
+    const qnt = args[1]
+
+    for(let i = 0; i < qnt; i++){
+        msg.channel.send(`Parabéns ${aniversariante}`)
+    }
+}
+
+
+module.exports = { command, execute }
